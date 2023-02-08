2b941517d9f1493f0ef5bb8ee664d9eb2eb0fe20
Arezoogh <ghorbanzadeharezoo@gmail.com>
2023-02-08 22:19:54 +0330

    practice2

--- practice2/index.js
+++
@@ -1,9 +0,0 @@
-const events = require('events')
-const EventEmitter = new events.EventEmitter()
-
-const myEventHandler = function(){
-    console.log('I sense a scream')
-}
-
-EventEmitter.on('scream', myEventHandler)
-EventEmitter.emit('scream')
